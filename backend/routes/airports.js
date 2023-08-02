41a66d3cd38d248aa90a7f09207078f49691e57c
bahaa1985 <bahaa_salaheldin@live.com>
2023-08-02 15:38:21 +0300

    airports 2 Aug

--- backend/routes/airports.js
+++ backend/routes/airports.js
@@ -1,8 +1,11 @@
-import { Router } from "express";
+import express from "express";
 import * as airport_model from '../admin/airport.js'
-const router=Router();
+import bodyParser from "body-parser";
+import mongoose, { MongooseError } from "mongoose";
+const urlEncoded=bodyParser.urlencoded({extended:false})
+const airportRouter=express.Router();
 
-export const airport_router=router.get('/',(req,res)=>{
+airportRouter.get('/',(req,res)=>{
     try{
         airport_model.getAirports().then(
          (data)=>{
@@ -10,8 +13,32 @@ export const airport_router=router.get('/',(req,res)=>{
          }
         )         
      }
-     catch{
-         res.status(500).json(Error)
+     catch(error){
+         res.status(500).send(error)
      }
 })
+.post('/newAirport',urlEncoded,(req,res)=>{    
+    const name=req.body.name
+    const code=req.body.code
+    // res.send(req.body)
+    airport_model.insertAirport(name,code).then(()=>{
+        res.status(200).send("Airport is created")
+        if(!name){
+            // throw createhttpErrors(400,) setup http-errors
+        }
+    })
+    .catch((error)=>{
+        res.status(500).send(error.message)
+    })
+    // try {
+       
+    // } catch  {
+    //     // res.send()
+    //     // next(error)
+    //     throw Error
+    // }
+    // res.send("post new airport")
+})
+
+export default airportRouter
 
