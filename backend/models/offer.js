2ee518f7067b77a5eb5c2cba918d5ebcdc85c1ba
bahaa1985 <bahaa_salaheldin@live.com>
2023-08-06 16:26:36 +0300

    offers 6 Aug v2

--- backend/models/offer.js
+++ backend/models/offer.js
@@ -3,7 +3,7 @@ import { Schema , Types} from "mongoose";
 const schema_offer=new Schema({
     agencyId:{type:Types.ObjectId,required:true},date:{type:String,required:true,default:new Date().toDateString()},
     departureId:{type:Types.ObjectId,required:true},arriveId:{type:Types.ObjectId,required:true},
-    travelDate:{type:Date,required:true},transporterId:{type:Types.ObjectId,require:true},
+    travelDate:{type:String,required:true},transporterId:{type:Types.ObjectId,require:true},
     seats:{type:Number,required:true,min:[1,"عدد المقاعد يجب أن لا يقل عن 1"]},
     price:{type:Number,required:true,min:[900,"السعر لا يجب ان يقل عن 900 جنيه"]},discount:{type:Number,min:[0,"الخصم غير صحيح"]}
 })
