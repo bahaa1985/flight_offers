75b043124367f15f186792acf35165cbdf94e341
bahaa1985 <bahaa_salaheldin@live.com>
2023-09-08 17:27:03 +0300

    user v5

--- frontend/src/views/registration.js
+++ frontend/src/views/registration.js
@@ -27,10 +27,10 @@ export default function Registration(){
             <button type="button" 
                     className="btn btn-primary"
                     data-bs-toggle='modal' data-bs-target='#myRegModal' 
-                    onClick={()=>[setFormState('new'),setName(''),setEmail(''),setPass(''),setMobile('')]}>
+                    onClick={()=>[setFormState('new'),setName(''),setEmail(''),setMobile('')]}>
                 مستخدم جديد<i className="bi bi-plus-lg"></i>
             </button>
-                                     
+            
             <table id="usersTable" className='table table-hover' >
                 <thead>
                     
@@ -39,33 +39,35 @@ export default function Registration(){
                     {
                         users.map((user,index)=>{
                             return(
-                            <tr key={index} >
-                                <td>{user.name}</td>                             
+                            <tr key={index}>
+                                <td>{user.name}</td>
+                                <td>{user.username}</td>
                                 <td>{user.email}</td>
                                 <td>{user.mobile}</td>
-                                <td>{user.user_type}</td>
+                                <td>{user.user_type}</td>                                
                                 <td>
-                                        <button type='submit' className='btn btn-success'                                                                               
-                                            onClick={() => [setFormState('update'),setId(user._id), 
-                                            setName(user.name), setEmail(user.email),
-                                            setMobile(user.mobile),setId(user._id)]}
-                                            data-bs-toggle='modal' data-bs-target='#myRegModal'
-                                            >
-                                            تعديل
-                                        </button>
-                                    </td>
-                                    <td>
-                                        <button type='submit' className='btn btn-danger'
-                                             onClick={() => [setFormState('delete'), setName(user.name), setId(user._id)]}
-                                            data-bs-toggle='modal' data-bs-target='#myRegModal'>
-                                            حذف
-                                        </button>
-                                    </td>                                   
+                                    <button type='submit' className='btn btn-success'
+                                        onClick={() => [setFormState('update'), 
+                                        setName(user.name), setEmail(user.email),
+                                        setMobile(user.mobile),setUsertype(user.user_type),setId(user._id)]}
+                                        data-bs-toggle='modal' data-bs-target='#myRegModal'
+                                    >
+                                        تعديل
+                                    </button>
+                                </td>
+                                <td>
+                                    <button type='button' className='btn btn-danger'
+                                            onClick={() => [setFormState('delete'), setName(user.name), setId(user._id)]}
+                                        data-bs-toggle='modal' data-bs-target='#myRegModal'>
+                                        حذف
+                                    </button>
+                                </td>
                             </tr>)
                         })
                     }
                 </tbody>
-            </table>          
+            </table>
+
             {/* bootstrap form within modal shown only when click new,update,delete buttons */}
             <div className='modal fade' id='myRegModal' dir="rtl">
                 <div className="modal-dialog">
@@ -81,32 +83,39 @@ export default function Registration(){
                             </h4>
                         </div>
                         <div className='modal-body'>
-                            <form className="form" onSubmit={()=>newUser(name,email,mobile,password,userType)} >  
+                            <form   className="form" 
+                                    onSubmit={()=>
+                                    // formState === 'new' ? ()=> newUser(name,email,mobile,password,userType) :
+                                    // formState === 'update' ? ()=> 
+                                    updateUser (id,name,email,mobile,userType)
+                                    // : null
+                                    }
+                            >
                                 {
-                                   formState === 'update' ? <label className="form-control" htmlFor="name_input">اسم المستخدم</label> :null
+                                    formState ==='update' ?  <label className="form-label" htmlFor="user_input">اسم المستخدم</label> : null
                                 }
-                                <input name="name_input" type="text" className="form-control mb-2" placeholder="اسم المستخدم" value={name} onChange={(e)=>e.target.value}/>                                                        
+                                <input name="user_input" type="text" className="form-control mb-2" placeholder="اسم المستخدم" value={name} onChange={(e)=>e.target.value}/>                            
                                 {
-                                   formState === 'update' ? <label className="form-control" htmlFor="email_input">الايميل</label> :null
+                                    formState ==='update' ?   <label className="form-label" htmlFor="email_input">الايميل</label> : null
                                 }
-                                <input name="email_input" type="email" className="form-control mb-2" placeholder="الايميل" value={email} onChange={(e)=>e.target.value}/>                               
+                                <input name="email_input" type="email" className="form-control mb-2" placeholder="الايميل" value={email} onChange={(e)=>e.target.value}/>
                                 {
-                                   formState === 'update' ? <label className="form-control" htmlFor="mobile_input">الموبايل</label> :null
+                                    formState  ==='update' ?   <label className="form-label" htmlFor="mobile_input">الموبايل</label> :null
                                 }
                                 <input name="mobile_input" type="text" className="form-control mb-2" placeholder="الموبايل" value={mobile} onChange={(e)=>e.target.value}/>
                                 {
                                     formState==='new' ?                                        
-                                        <Fragment>                                          
-                                            <input type="password" className="form-control mb-2" placeholder="الباسوورد" onChange={(e)=>e.target.value}/>                                          
-                                            <input type="password" className="form-control mb-2" placeholder="تأكيد الباسوورد" onChange={(e)=>e.target.value}/>
+                                        <Fragment>
+                                            <label className="form-control" htmlFor="pass_input">الباسوورد</label>
+                                            <input type="password" className="form-control mb-2" onChange={(e)=>e.target.value}/>
+                                            <label className="form-control" htmlFor="confirm_input">تأكيد الباسوورد</label>
+                                            <input type="password" className="form-control mb-2" onChange={(e)=>e.target.value}/>
                                         </Fragment>
                                     :null
                                 }
-                               {
-                                   formState === 'update' ? <label className="form-control" htmlFor="select_input">نوع المستخدم</label> :null
-                                }
-                               <label className="form-control" htmlFor="select_input"></label>
-                                <select className="form-control mb-2" onChange={(e)=>setUsertype(e.target.value)}>
+                               
+                                <label className="form-label" htmlFor="select_input">نوع المستخدم</label>
+                                <select name="select_input" className="form-control mb-2" onSelect={(e)=>setUsertype(e.target.value)} defaultValue={userType}>
                                     <option value="ادمن">ادمن</option>
                                     <option value="وكيل">وكيل</option>
                                     <option value="شركة">شركة</option>
